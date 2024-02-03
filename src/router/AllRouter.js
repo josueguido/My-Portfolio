334fa4be351332c58f798f9d8ffb47d45d82b61d
Your Name <you@example.com>
2024-02-03 14:54:43 -0600

    Correccion de dependecias

--- src/router/AllRouter.js
+++ src/router/AllRouter.js
@@ -4,14 +4,12 @@ import NotFound from "../views/NotFound";
 import { Routes, Route } from "react-router-dom";
 
 const AllRouter = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/Home" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </>
-  );
+    return (
+        <Routes>
+            <Route path="/home" element={<Home />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes>
+    );
 };
 
 export default AllRouter;
